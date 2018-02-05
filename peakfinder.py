c4320db9498d119ab41a874ac190c5dc05516860
ird <31051194+ird@users.noreply.github.com>
2018-02-05 22:07:44 +0000

    added linear_peak_finder(L)

--- peakfinder.py
+++ peakfinder.py
@@ -1,24 +1,22 @@
-def list_peak_finder(L, n=0):
+def list_peak_finder(L, start=0, end=None):
     """
     A peak exists at n L[n] where L[n] >= L[n-1], L[n+1].
     If L[n] only has one neighbour, then it is sufficient that it is greater
     than that neighbour only.
 
-    Returns the index of the peak. 
-    
+    Returns the peak.
     """
-    # exit criteria
-    
-    # pick the middle of the list
-    
-    if n > 0 and L[n-1] >= L[n]:
-        return list_peak_finder(L, n-1)
-    if n < len(L)-1 and L[n+1] >= L[n]:
-        return list_peak_finder(L, n+1)
-    return n
+    if end == None:
+        end = len(L) - 1
+    n = start + (end - start >> 1)
+    if n > start and L[n-1] >= L[n]:
+        return list_peak_finder(L, start, n-1)
+    if n < end and L[n+1] >= L[n]:
+        return list_peak_finder(L, n+1, end)
+    return L[n]
 
 def main():
-    pass
+    print(list_peak_finder([5,6,7,8,82,1]))
 
 if __name__ == "__main__":
     main()   
