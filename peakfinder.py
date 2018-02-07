41d7f4e8cff6a9e78287f055e0a788cf1d349779
ird <31051194+ird@users.noreply.github.com>
2018-02-07 20:34:59 +0000

    made list_peak_finder() handle short lists more explicitly

--- peakfinder.py
+++ peakfinder.py
@@ -1,22 +1,29 @@
+
 def list_peak_finder(L, start=0, end=None):
     """
-    A peak exists at n L[n] where L[n] >= L[n-1], L[n+1].
+    Definition: A peak exists at n L[n] where L[n] >= L[n-1], L[n+1].
     If L[n] only has one neighbour, then it is sufficient that it is greater
     than that neighbour only.
-
     Returns the peak.
     """
     if end == None:
-        end = len(L) - 1
-    n = start + (end - start >> 1)
-    if n > start and L[n-1] >= L[n]:
-        return list_peak_finder(L, start, n-1)
-    if n < end and L[n+1] >= L[n]:
-        return list_peak_finder(L, n+1, end)
+        end = len(L)
+    size = end - start
+    n = start + (size >> 1)
+    if size > 2:
+        if L[n-1] >= L[n]:
+            return list_peak_finder(L, start, n)
+        if L[n+1] >= L[n]:
+            return list_peak_finder(L, n+1, end)
+    if size == 1:
+        pass
+    if size == 2:
+        if L[n-1] >= L[n]:
+            return L[n-1]
     return L[n]
 
 def main():
-    print(list_peak_finder([5,6,7,8,82,1]))
+    print(list_peak_finder([1,2,3,4,6,7]))
 
 if __name__ == "__main__":
-    main()   
+    main()
