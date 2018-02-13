443632e733942de5a42134a2bf88031233c0b969
ird <31051194+ird@users.noreply.github.com>
2018-02-13 20:48:32 +0000

    iterative version of max_heapify() used instead

--- heaps.py
+++ heaps.py
@@ -9,30 +9,57 @@ def max_heapify(heap, root, size):
     at the heap's root.
     """
     l = 2*root+1
-    r = 2*root+2
     if size <= l:
         # no children at root
         return
-    elif size <= r:
+    r = l+1
+    if size <= r:
         # only a left child
         if heap[l] > heap[root]:
             heap[l], heap[root] = heap[root], heap[l]
             return max_heapify(heap, l, size)
-        else:
-            return
     else:
         if heap[l] > heap[r]:
             if heap[l] > heap[root]:
                 heap[l], heap[root] = heap[root], heap[l]
                 return max_heapify(heap, l, size)
-            else:
-                return
         else:
             if heap[r] > heap[root]:
                 heap[r], heap[root] = heap[root], heap[r]
                 return max_heapify(heap, r, size)
+
+def max_heapify_iter(heap, root=0):
+    """
+    Non-recursive version of max_heapify().
+    Pre-condition: the trees anchored at left(heap,0) and right(heap,0) are already max-heaps
+    Returns heap as a max-heap in O(lg n) by correcting the violation of the max-heap-property
+    at the heap's root.
+    """
+    size = len(heap)
+    while True:
+        l = 2*root+1
+        if size <= l:
+            # no children at root
+            break
+        r = l+1
+        if size <= r:
+            # only a left child
+            if heap[l] > heap[root]:
+                heap[l], heap[root] = heap[root], heap[l]
+                root = l
+                continue
+        else:
+            if heap[l] > heap[r]:
+                if heap[l] > heap[root]:
+                    heap[l], heap[root] = heap[root], heap[l]
+                    root = l
+                    continue
             else:
-                return
+                if heap[r] > heap[root]:
+                    heap[r], heap[root] = heap[root], heap[r]
+                    root = r
+                    continue
+        break
 
 def build_heap(array):
     """
@@ -42,7 +69,7 @@ def build_heap(array):
     size = len(heap)
     n = size >> 1
     for i in range(n, -1, -1):
-        max_heapify(heap, i, size)
+        max_heapify_iter(heap, i)
     return heap
 
 def extract_max(heap):
@@ -52,7 +79,7 @@ def extract_max(heap):
     max = heap[0]
     heap[0] = heap[len(heap)-1]
     heap.pop()
-    max_heapify(heap, 0, len(heap))
+    max_heapify_iter(heap)
     return max
 
 def heapsort(array):
@@ -72,14 +99,14 @@ def main():
 def test():
     # run "py.test heaps.py"
     heap = [4, 5, 0]
-    max_heapify(heap, 0, len(heap))
+    max_heapify_iter(heap)
     assert(heap == [5, 4, 0])
     heap = [1, 1, 3]
-    max_heapify(heap, 0, len(heap))
+    max_heapify_iter(heap)
     assert(heap == [3, 1, 1])
-    heap = [10, 11, 8, 16, 17]
-    max_heapify(heap, 0, len(heap))
-    assert(heap == [11, 17, 8, 16, 10])
+    heap = [10, 12, 8, 6, 11]
+    max_heapify_iter(heap)
+    assert(heap == [12, 11, 8, 6, 10])
     assert(build_heap([4,15,11,7,6,18,2]) == [18,15,11,7,6,4,2])
     array = [1,2,3,4,5,6,7,8,9,10,11]
     heapsort(array)
