c59a11e666e9b077dce44cdf1f6f0fefabc7dac7
ird <31051194+ird@users.noreply.github.com>
2018-02-13 20:19:56 +0000

    removal of slow quick_select() and select_new_root() for inline method

--- heaps.py
+++ heaps.py
@@ -2,120 +2,47 @@ from timeit import default_timer as timer
 from random import randint
 import cProfile
 
-def left(heap, node):
-    """
-    Returns the index of the root of the left sub-tree of a heap implemented as an array
-    A[2n+1] = root of left sub tree of A[n]
-    """
-    l = 2*node+1
-    if l > len(heap):
-        raise IndexError
-    if heap[l] == None:
-        raise IndexError
-    return l
-
-def right(heap, node):
-    """
-    Returns the index of the root of the left sub-tree of a heap implemented as an array
-    A[2n+1] = root of left sub tree of A[n]
-    """
-    r = 2*node+2
-    if r > len(heap):
-        raise IndexError
-    if heap[r] == None:
-        raise IndexError
-    return r
-
-def max_heapify(heap, root):
+def max_heapify(heap, root, size):
     """
     Pre-condition: the trees anchored at left(heap,0) and right(heap,0) are already max-heaps
     Returns heap as a max-heap in O(lg n) by correcting the violation of the max-heap-property
     at the heap's root.
     """
-    largest = quick_select(heap, root)
-    if largest == "root":
-        # print("nothing left to change")
-        return
-    if largest == "left":
-        l = 2*root+1
-        # print("at", root, "new root should be (left)", l)
-        t = heap[l]
-        heap[l] = heap[root]
-        heap[root] = t
-        return max_heapify(heap, l)
-    if largest == "right":
-        r = 2*root+2
-        # print("at", root, "new root should be (right)", r)
-        t = heap[r]
-        heap[r] = heap[root]
-        heap[root] = t
-        return max_heapify(heap, r)
-
-def quick_select(heap, root):
-    """
-    version of select_new_root(), below, that doesn't use exceptions, for speed
-    profiler shows 85% of time spent in select_new_root()
-    """
     l = 2*root+1
-    if l >= len(heap):
-        left_branch = False
-    else:
-        left_branch = True
     r = 2*root+2
-    if r >= len(heap):
-        right_branch = False
-    else:
-        right_branch = True
-
-    if left_branch and right_branch:
-        if heap[l] >= heap[r]:
-            return "root" if heap[root] >= heap[l] else "left"
+    if size <= l:
+        # no children at root
+        return
+    elif size <= r:
+        # only a left child
+        if heap[l] > heap[root]:
+            heap[l], heap[root] = heap[root], heap[l]
+            return max_heapify(heap, l, size)
         else:
-            return "root" if heap[root] >= heap[r] else "right"
-    if not left_branch or not right_branch:
-        return "root"
-
-    if left_branch:
-        return "root" if heap[root] >= heap[l] else "left"
-    if right_branch:
-        return "root" if heap[root] >= heap[r] else "right"
-
-def select_new_root(heap, root):
-    """
-    select_new_root() is a utility function that returns "root", "left" or "right" depending on
-    whether the existing root, left sub-tree root or right sub-tree root is largest, respectively.
-    This prevents calling left(heap, root) [etc] if one does not exist as this function handles
-    IndexErrors as they occur.
-    """
-    try:
-        l = left(heap, root)
-        try:
-            r = right(heap, root)
-            if heap[l] >= heap[r]:
-                # print("left is bigger than right, so testing heap[l]")
-                return "root" if heap[root] >= heap[l] else "left"
+            return
+    else:
+        if heap[l] > heap[r]:
+            if heap[l] > heap[root]:
+                heap[l], heap[root] = heap[root], heap[l]
+                return max_heapify(heap, l, size)
             else:
-                # print("right is bigger than left, so testing heap[r]")
-                return "root" if heap[root] >= heap[r] else "right"
-        except:
-            # print("there is no right sub-tree")
-            return "root" if heap[root] >= heap[l] else "left"
-    except:
-        try:
-            # print("there is no left sub-tree")
-            return "root" if heap[root] >= heap[r] else "right"
-        except:
-            # print("there are no leaves")
-            return "root" # there are no children
+                return
+        else:
+            if heap[r] > heap[root]:
+                heap[r], heap[root] = heap[root], heap[r]
+                return max_heapify(heap, r, size)
+            else:
+                return
 
 def build_heap(array):
     """
     Build a max heap from an unsorted array in O(n)
     """
     heap = array[:]
-    n = len(heap) >> 1
+    size = len(heap)
+    n = size >> 1
     for i in range(n, -1, -1):
-        max_heapify(heap, i)
+        max_heapify(heap, i, size)
     return heap
 
 def extract_max(heap):
@@ -125,7 +52,7 @@ def extract_max(heap):
     max = heap[0]
     heap[0] = heap[len(heap)-1]
     heap.pop()
-    max_heapify(heap, 0)
+    max_heapify(heap, 0, len(heap))
     return max
 
 def heapsort(array):
@@ -145,25 +72,19 @@ def main():
 def test():
     # run "py.test heaps.py"
     heap = [4, 5, 0]
-    assert(select_new_root(heap, 0) == "left")
-    assert(quick_select(heap, 0) == "left")
-    max_heapify(heap, 0)
+    max_heapify(heap, 0, len(heap))
     assert(heap == [5, 4, 0])
     heap = [1, 1, 3]
-    assert(select_new_root(heap, 0) == "right")
-    assert(quick_select(heap, 0) == "right")
-    max_heapify(heap, 0)
+    max_heapify(heap, 0, len(heap))
     assert(heap == [3, 1, 1])
-    heap = [10, 4, 5]
-    assert(select_new_root(heap, 1) == "root")
-    assert(quick_select(heap, 0) == "root")
     heap = [10, 11, 8, 16, 17]
-    max_heapify(heap, 0)
+    max_heapify(heap, 0, len(heap))
     assert(heap == [11, 17, 8, 16, 10])
     assert(build_heap([4,15,11,7,6,18,2]) == [18,15,11,7,6,4,2])
     array = [1,2,3,4,5,6,7,8,9,10,11]
     heapsort(array)
     assert(array == [11,10,9,8,7,6,5,4,3,2,1])
+
 
 if __name__ == "__main__":
     main()
